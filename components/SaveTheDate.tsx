4e59531095574d6aad030428d5df7c90d6cb11c6
Vinay <vnaycreative@gmail.com>
2025-10-23 01:59:33 +0530

    feat: Main page hero section done

--- components/SaveTheDate.tsx
+++ components/SaveTheDate.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { motion } from "framer-motion"
-import { BookHeart, CalendarHeart, Images } from "lucide-react"
+import { BookHeart, CalendarHeart } from "lucide-react"
 import Image from "next/image"
 
 const SaveTheDate = () => {
